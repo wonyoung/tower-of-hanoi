1b1c99e07bf1262b97fa6e64affc2c0b89d320e1
Wonyoung Jang <wonyoungjjang@gmail.com>
2019-11-24 20:59:23 +0900

    first commit of hanoi-tower

--- src/App.js
+++ src/App.js
@@ -1,24 +1,115 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import solveHanoi from "./solveHanoi";
+import "./App.css";
+
+function createTower(n) {
+  return [...Array(n)].map((_, i) => n - i);
+}
+
+function createTowers(h) {
+  return [createTower(h), [], []];
+}
+
+function move(blocks, from, to) {
+  blocks[to].push(blocks[from].pop());
+  return [...blocks];
+}
+
+function Block({ value, ...props }) {
+  return (
+    <div {...props} className="hanoi-block" style={{ width: value * 24 }}></div>
+  );
+}
+
+function Bottom({ index, ...props }) {
+  return (
+    <>
+      <div className="hanoi-bottom"></div>
+      <div className="hanoi-desc">{index}</div>
+    </>
+  );
+}
+
+function HanoiTower({ blocks, ...props }) {
+  const height = blocks.reduce((acc, l) => acc + l.length, 0);
+  const xs = [...Array(blocks.length)].map((_, x) => {
+    const h = blocks[x].length;
+    return (
+      <div key={x} className="hanoi-block-container">
+        {[...Array(height)].map((_, y) => {
+          const idx = height - y - 1;
+          return <Block key={idx} value={idx < h ? blocks[x][idx] : 0} />;
+        })}
+        <Bottom index={x + 1} />
+      </div>
+    );
+  });
+  return <div className="hanoi-container">{xs}</div>;
+}
+
+function Solutions({ value, cursor, ...props }) {
+  return (
+    <div {...props}>
+      {value.map(({ num, from, to }, i) => (
+        <div
+          key={i}
+          className={i === cursor ? "sol-highlight" : "sol-normal"}
+        >{`${num} : ${from} --> ${to}`}</div>
+      ))}
+    </div>
+  );
+}
 
 function App() {
+  const initialBlockNum = 3;
+  const [blocks, setBlocks] = useState(createTowers(initialBlockNum));
+  const [cursor, setCursor] = useState(0);
+  const [solution, setSolution] = useState(solveHanoi(initialBlockNum));
+
+  const handleInputNum = e => {
+    if (e.keyCode === 13) {
+      const num = Number(e.target.value);
+      if (!isNaN(num) && num < 11) {
+        const blks = createTowers(num);
+        setBlocks(blks);
+        setCursor(0);
+        setSolution(solveHanoi(num));
+      }
+      e.target.value = "";
+    }
+  };
+
+  const handleLeft = e => {
+    if (cursor > 0) {
+      const { from, to } = solution[cursor - 1];
+      setBlocks(move(blocks, to - 1, from - 1));
+      setCursor(cursor - 1);
+    }
+  };
+  const handleRight = e => {
+    if (cursor < solution.length) {
+      const { from, to } = solution[cursor];
+      setBlocks(move(blocks, from - 1, to - 1));
+      setCursor(cursor + 1);
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">Tower of Hanoi</div>
+      <div className="App-body">
+        <HanoiTower blocks={blocks} />
+      </div>
+      <div className="App-tail">
+        <div>
+          Height : <input onKeyDown={handleInputNum} />
+        </div>
+        <div>
+          {`Move `}
+          <button onClick={handleLeft}>{"<"}</button>
+          <button onClick={handleRight}>{">"}</button>
+        </div>
+        <Solutions value={solution} cursor={cursor} />
+      </div>
     </div>
   );
 }
